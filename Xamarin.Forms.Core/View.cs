db78b5b6cd38d5f20fa767733980aaf02d7c5200
Louis <luqingfei@126.com>
2016-07-30 11:07:33 +0800

    add some comment for View class

--- Xamarin.Forms.Core/View.cs
+++ Xamarin.Forms.Core/View.cs
@@ -7,6 +7,21 @@ using Xamarin.Forms.Internals;
 
 namespace Xamarin.Forms
 {
+    /// <summary>
+    /// A visual element that is used to place layouts and controls on the screen.
+    /// 一个可视化元素通常用来在屏幕上放置布局和控件。
+    /// </summary>
+    /// <remarks>
+    /// https://developer.xamarin.com/api/type/Xamarin.Forms.View/
+    /// The View class is a base class for the Layout class, and most of the controls that application developers will use. 
+    /// View类作为布局类和开发者使用的大多数控件的基类。
+    /// Nearly all UI elements that an application developer will use are derived from View class. 
+    /// 开发者将使用的几乎所有的UI元素都继承自View类。
+    /// Because the View class ultimately inherits from BindableObject class, application developers can use the Model-View-ViewModel architecture, 
+    /// 因为View类最终继承自BindableObject类，开发者可以使用MVVM构架。
+    /// as well as XAML, to develop portable user interfaces. 
+    /// 和XAML一样，可以开发可移植的用户界面。
+    /// </remarks>
 	public class View : VisualElement, IViewController
 	{
 		public static readonly BindableProperty VerticalOptionsProperty = BindableProperty.Create("VerticalOptions", typeof(LayoutOptions), typeof(View), LayoutOptions.Fill,
@@ -64,6 +79,9 @@ namespace Xamarin.Forms
 			set { SetValue(HorizontalOptionsProperty, value); }
 		}
 
+        /// <summary>
+        ///  Gets or sets the margin for the view.
+        /// </summary>
 		public Thickness Margin
 		{
 			get { return (Thickness)GetValue(MarginProperty); }
